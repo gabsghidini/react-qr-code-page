2179d2365e995771f2b526c1dc60fc2637664835
Gabs Ghidini <43341520+gabsghidini@users.noreply.github.com>
2022-08-18 09:55:19 -0300

    Style: Mobile First using CSS and React

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="container">
+					<img
+						className="qr-code"
+						src="./assets/imgs/image-qr-code.png"
+						alt="QR Code"
+					/>
+					<h4 className="title">
+						Improve your front-end skills by building projects
+					</h4>
+					<p className="text">
+						Scan the QR code to visit Frontend Mentor and take your
+						coding skills to the next level
+					</p>
+				</div>
+			</header>
+		</div>
+	);
+}
+
+export default App;
